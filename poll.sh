9cbb6d2515a420c2323f810370e56c1c1474f05d
Jared Engler <jared.lite@gmail.com>
2021-02-22 00:11:21 -0600

    readd title, escape control characters in jq

--- poll.sh
+++ poll.sh
@@ -6,9 +6,9 @@ cat usernames |\
          GET \"https://api.twitch.tv/helix/search/channels?query=@\" \
          --header \"client-id: ${CLIENT_ID}\" \
          --header \"Authorization: Bearer ${OAUTH}\" \
-         | jq -r '.data[] | select(.display_name==\"@\") | select(.is_live==true) | .display_name' " \
-    | while read name; do \
-        echo "$name" \
+         | jq -r '.data[] | select(.display_name==\"@\") | select(.is_live==true) | .display_name, .title | gsub(\"[\\n\\t]\"; \"\")' " \
+    | while read name; do read title; \
+        echo "$name: $title" \
         && curl --silent --request POST "${DISCORD_WEBHOOK}" --header 'Content-type: application/json' \
-            -d "{\"username\":\"laggybot\",\"avatar_url\":\"\",\"content\":\"$name is live (<https://twitch.tv/$name>)\"}" \
+            -d "{\"username\":\"laggybot\",\"avatar_url\":\"\",\"content\":\"$name is live: $title (<https://twitch.tv/$name>)\"}" \
         ; done
